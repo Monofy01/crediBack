ced85418fddccc5d47bb225ab7ca9c85eff6f58a
monofy01 <bri_riva@hotmail.com>
2022-08-19 13:55:17 -0500

    [INIT] init project

---
+++ src/main/java/com/brian/crediback/CrediBackApplication.java
@@ -0,0 +1,13 @@
+package com.brian.crediback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrediBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brian/crediback/CrediBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.crediback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
